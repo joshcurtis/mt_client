c2bfce0f59400d4bdb2e53cc950d558652cab378
Josh Curtis <jcurtis825@gmail.com>
2016-03-28 19:08:58 -0700

    Make things work

--- start.js
+++ start.js
@@ -1,7 +1,6 @@
 var mt = require('machinetalk-protobuf');
 var zmq = require('zmq');
 
-
 var trim = function(msg) {
     for (var key in msg) {
 	if (msg[key] === null) {
@@ -14,46 +13,51 @@ var trim = function(msg) {
     return msg
 }
 
-launcher_port = '62285'
-launchercmd_port = '50298'
+var PING = {
+    type: mt.message.ContainerType.MT_PING
+}
+var PING_BUF = mt.message.Container.encode(PING)
+PING_BUF = PING_BUF.buffer.slice(0, PING_BUF.limit)
+
+
+launcher_port = '63121'
+launchercmd_port = '50356'
 var started = false
 
 // Create sub socket
 var socket_sub = zmq.socket("sub")
-socket_sub.identity = "joshs-mac"
+socket_sub.identity = "socket subscriber"
 socket_sub.connect("tcp://134.197.40.182:" + launcher_port)
 
 var dealer = zmq.socket('dealer')
-dealer.identity = 'joshs-mac'
+dealer.identity = 'dealer'
 dealer.connect("tcp://134.197.40.182:" + launchercmd_port)
 
 socket_sub.on('message', function(topic, msg) {
-    var ping = {
-	type: mt.message.ContainerType.MT_PING
-    }
-    var ping_buf = mt.message.Container.encode(ping)
-    ping_buf = ping_buf.buffer.slice(0, msg.ping_buf)
-    dealer.send(ping_buf)
-
+    msg = trim(mt.message.Container.decode(msg))
+    console.log(msg)
+    dealer.send(PING_BUF)
 
     if( !started ) {
+	console.log(msg)
 	var start = {
 	    type: mt.message.ContainerType.MT_LAUNCHER_START,
-	    index: msg["index"]
+	    index: msg["launcher"][0]["index"]
 	}
-
+	console.log(start)
 	var start_buf = mt.message.Container.encode(start)
-	start_buf = start_buf.buffer.slice(0, start.limit)
+	start_buf = start_buf.buffer.slice(0, start_buf.limit)
 	dealer.send(start_buf)
 	started = true
     }
 
-    console.log(msg)
+
+
 })
 
 dealer.on('message', function(msg) {
     msg = trim(mt.message.Container.decode(msg))
-    console.log(msg)
+//    console.log(msg)
 })
 
 console.log("Starting machinekit")
