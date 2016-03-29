9b653511113635fc63c9b293ee367700e5135920
Josh Curtis <jcurtis825@gmail.com>
2016-03-29 16:40:30 -0700

    Use utils

--- ping.js
+++ ping.js
@@ -1,23 +1,10 @@
 var mt = require('machinetalk-protobuf');
 var zmq = require('zmq');
+var utils = require('./utils.js');
 
 var Container = mt.message.Container
 
-
-var trim = function(msg) {
-    for (var key in msg) {
-	if (msg[key] === null) {
-	    delete msg[key]
-	} else if (Object.prototype.toString.call(msg[key]) === '[object Array]' &&
-		   msg[key].length === 0) {
-	    delete msg[key]
-	}
-    }
-    return msg
-}
-
-
-var port = '65159'
+var port = '64272'
 var dealer = zmq.socket('dealer')
 dealer.identity = 'batman'
 dealer.connect('tcp://134.197.40.182:' + port)
@@ -33,5 +20,5 @@ setInterval(function() {
 }, 5000)
 
 dealer.on('message', function(msg) {
-    console.log(trim(Container.decode(msg)))
+    console.log(utils.trim(Container.decode(msg)))
 })
--- shutdown.js
+++ shutdown.js
@@ -1,17 +1,7 @@
 var mt = require('machinetalk-protobuf');
 var zmq = require('zmq');
+var utils = require('utils');
 var Container = mt.message.Container
-var trim = function(msg) {
-    for (var key in msg) {
-	if (msg[key] === null) {
-	    delete msg[key]
-	} else if (Object.prototype.toString.call(msg[key]) === '[object Array]' &&
-		   msg[key].length === 0) {
-	    delete msg[key]
-	}
-    }
-    return msg
-}
 
 // This should be port of service command
 var port = '51949'
@@ -28,6 +18,6 @@ BUFFER = BUFFER.buffer.slice(0, BUFFER.limit)
 dealer.send(BUFFER)
 
 dealer.on('message', function(msg) {
-    console.log(trim(Container.decode(msg)))
+    console.log(utils.trim(Container.decode(msg)))
     console.log("Shutting down")
 })
