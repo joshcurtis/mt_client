f8d19734939ad278ff0c245b3907b24941666189
Josh Curtis <jcurtis825@gmail.com>
2016-03-29 15:42:47 -0700

    Improved ping.js

--- ping.js
+++ ping.js
@@ -2,26 +2,36 @@ var mt = require('machinetalk-protobuf');
 var zmq = require('zmq');
 
 var Container = mt.message.Container
-var port = '53754'
-var dealer = zmq.socket('dealer')
-dealer.identity = 'joshs-mac'
-dealer.onReady = function() {
-    console.log("ready")
+
+
+var trim = function(msg) {
+    for (var key in msg) {
+	if (msg[key] === null) {
+	    delete msg[key]
+	} else if (Object.prototype.toString.call(msg[key]) === '[object Array]' &&
+		   msg[key].length === 0) {
+	    delete msg[key]
+	}
+    }
+    return msg
 }
 
-dealer.connect('tcp://134.197.40.182:' + port)
 
-dealer.on('message', function(msg) {
-    console.log(Container.decode(msg))
-})
+var port = '65159'
+var dealer = zmq.socket('dealer')
+dealer.identity = 'batman'
+dealer.connect('tcp://134.197.40.182:' + port)
 
 var PING = {
     type: mt.message.ContainerType.MT_PING
 }
+var BUFFER = Container.encode(PING)
+BUFFER = BUFFER.buffer.slice(0, BUFFER.limit)
 
-var PING_BUF = Container.encode(PING)
-PING_BUF = PING_BUF.buffer.slice(0,3)
-setInterval( function() {
-    console.log("Pinging")
-    dealer.send(PING_BUF)
-}, 2000)
+setInterval(function() {
+    dealer.send(BUFFER)
+}, 5000)
+
+dealer.on('message', function(msg) {
+    console.log(trim(Container.decode(msg)))
+})
